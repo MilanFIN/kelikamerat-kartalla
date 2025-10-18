d7f8673a698b89e3026356819c1495d8ff67812f
milanfin <milan.makipaa@gmail.com>
2025-10-18 20:10:09 +0300

    capacitor android port

---
+++ android/app/src/main/java/com/kelikamerat/kartalla/MainActivity.java
@@ -0,0 +1,5 @@
+package com.kelikamerat.kartalla;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kelikamerat.kartalla',
+  appName: 'Kelikamerat Kartalla',
+  webDir: 'dist'
+};
+
+export default config;
